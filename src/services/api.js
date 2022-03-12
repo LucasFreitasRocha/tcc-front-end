2c679e9b9ee23aa83228f92e2d69ffb855872a10
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-03-11 23:03:03 -0300

    removendo cors

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,4 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'https://tcc-back-spring.herokuapp.com/'
 });
-api.defaults.headers['Access-Control-Allow-Origin'] = '*';
 export default api;
