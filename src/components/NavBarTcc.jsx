ee5425b0570f29b96655f723bc4a2a4764ef9e80
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-07-20 21:51:06 -0300

    finalizando css menu

--- src/components/NavBarTcc.jsx
+++ src/components/NavBarTcc.jsx
@@ -1,20 +1,27 @@
-import * as React from 'react';
+import  React, { useState }  from 'react';
 import { Link } from 'react-router-dom';
 import "./index.css";
 
 const pages = [ {id:1 , label: 'turmas'},{id:2 , label: 'temas'} , {id:3 , label: 'ranking'} ];
-const settings = ['Profile',  'Logout'];
+
 
  const NavBarTcc = () => {
- 
-
+  const [activeMenu, setActiveMenu] = useState(false);
+  const handleActiveMenu = () => { 
+    setActiveMenu(!activeMenu)
+  }
   return (
     <header>
       <nav className="nav">
         <Link to={"/"} className="logo link">TCC</Link>
-        <ul className="link nav-list">
+        <div className={activeMenu ? 'mobile-menu active' : 'mobile-menu' }  onClick={handleActiveMenu}>
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+        </div>
+        <ul className={activeMenu ? 'nav-list active' : 'nav-list' } >
         {pages.map((page) => (
-          <li key={page.id} ><Link className="link" to={`/${page.label}`}>{page.label}</Link></li>
+          <li key={page.id}  className={activeMenu ? 'active' : ''}><Link className='link'  to={`/${page.label}`}>{page.label}</Link></li>
         ))}
         </ul>
       </nav>
