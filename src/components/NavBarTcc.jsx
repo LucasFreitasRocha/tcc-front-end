db10e9e383489bf8fd5186818204fd7b0f9f984f
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-08-04 20:45:57 -0300

    rota logoff

--- src/components/NavBarTcc.jsx
+++ src/components/NavBarTcc.jsx
@@ -1,38 +1,48 @@
-import  React, { useState , useEffect}  from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import "./index.css";
 
-const pages = [ {id:1 , label: 'turmas'},{id:2 , label: 'temas'} , {id:3 , label: 'ranking'} ];
+const pages = [{ id: 1, label: 'turmas' }, { id: 2, label: 'temas' }, { id: 3, label: 'ranking' }, { id: 4, label: 'Logoff' }];
 
 
- const NavBarTcc = () => {
+const NavBarTcc = () => {
   const [activeMenu, setActiveMenu] = useState(false);
   const jwt = localStorage.getItem('jwt');
   const history = useHistory();
-  
-  const handleActiveMenu = () => { 
+
+  const handleActiveMenu = () => {
     setActiveMenu(!activeMenu)
   }
 
   useEffect(() => {
     if (!jwt) {
       history.push("/login");
-    } 
+    }
 
-  },[history,jwt])
+  }, [history, jwt])
+  const handleLogoff = () => {
+    localStorage.setItem('jwt', '');
+    history.push("/login");
+  }
   return (
     <header>
       <nav className="nav">
         <Link to={"/"} className="logo link">TCC</Link>
-        <div className={activeMenu ? 'mobile-menu active' : 'mobile-menu' }  onClick={handleActiveMenu}>
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
+        <div className={activeMenu ? 'mobile-menu active' : 'mobile-menu'} onClick={handleActiveMenu}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
         </div>
-        <ul className={activeMenu ? 'nav-list active' : 'nav-list' } >
-        {pages.map((page) => (
-          <li key={page.id}  className={activeMenu ? 'active' : ''}><Link className='link'  to={`/${page.label}`}>{page.label}</Link></li>
+        <ul className={activeMenu ? 'nav-list active' : 'nav-list'} >
+          {pages.map((page) => (
+          
+              <li key={page.id} className={activeMenu ? 'active' : ''}>
+                {(page.label === 'Logoff')? 
+                <span className='link' onClick={handleLogoff}>{page.label} </span> :
+                <Link className='link' to={`/${page.label}`}>{page.label}</Link>
+                }
+              </li> 
         ))}
         </ul>
       </nav>
