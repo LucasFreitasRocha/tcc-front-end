874784a7e91ffb62ecb6f68f5939351c761fb33f
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-10-15 14:02:56 -0300

    mudando a url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    //baseURL: 'https://tcc-back-spring.herokuapp.com/'
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://tcc-back-spring.herokuapp.com/'
+    //baseURL: 'http://localhost:8080/'
 });
 export default api;
